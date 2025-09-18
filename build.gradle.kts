bc5d3209e6e26604d95cb5b1cf030cf2ea563181
B. K. Oxley (binkley) <binkley@alumni.rice.edu>
2025-09-18 10:06:04 -0500

    Introduce Koog

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,6 +2,11 @@ plugins {
     kotlin("jvm") version "2.2.20"
 }
 
+dependencies {
+    implementation(libs.koog.core)
+    implementation(libs.koog.gemini)
+}
+
 tasks.wrapper {
     distributionType = Wrapper.DistributionType.ALL
 }
